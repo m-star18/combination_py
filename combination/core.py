98a1615a2d4fd5dc144968e85ac06eafcd152bd4
m-star18 <ryuuchei@ezweb.ne.jp>
2020-08-01 22:50:40 +0900

    Add core.py

---
+++ combination/core.py
@@ -0,0 +1,16 @@
+class Combination:
+
+    def __init__(self, n_max, mod=10**9+7):
+        # O(n_max + log(mod))
+        f = 1
+        self.mod = mod
+        self.factorials = factorials = [f]
+        for i in range(1, n_max + 1):
+            f *= i % mod
+            factorials.append(f)
+        f = pow(f, mod - 2, mod)
+        self.invs = invs = [f]
+        for i in range(n_max, 0, -1):
+            f *= i % mod
+            invs.append(f)
+        invs.reverse()
