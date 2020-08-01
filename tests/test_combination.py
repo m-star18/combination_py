ecd97fc09afb72e494723ee669a36a000a412281
m-star18 <ryuuchei@ezweb.ne.jp>
2020-08-02 01:28:23 +0900

    test nPr

--- tests/test_combination.py
+++ tests/test_combination.py
@@ -13,6 +13,9 @@ class TestCombination(unittest.TestCase):
     def test_nCr(self):
         self.assertEqual(538992043, self.comb.nCr(100, 50))
 
+    def test_nPr(self):
+        self.assertEqual(505671657, self.comb.nPr(100, 50))
+
 
 if __name__ == '__main__':
     unittest.main()
