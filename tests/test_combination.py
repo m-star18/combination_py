a2b6021dc6eb9aa13e20ce62170463119e0d3719
m-star18 <ryuuchei@ezweb.ne.jp>
2020-08-02 01:30:46 +0900

    test nHr

--- tests/test_combination.py
+++ tests/test_combination.py
@@ -5,7 +5,7 @@ from combination import Combination
 
 class TestCombination(unittest.TestCase):
     def setUp(self):
-        self.comb = Combination(100)
+        self.comb = Combination(1000)
 
     def tearDown(self):
         pass
@@ -15,6 +15,9 @@ class TestCombination(unittest.TestCase):
 
     def test_nPr(self):
         self.assertEqual(505671657, self.comb.nPr(100, 50))
+
+    def test_nHr(self):
+        self.assertEqual(475860182, self.comb.nHr(100, 50))
 
 
 if __name__ == '__main__':
